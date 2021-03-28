c90691364d2e555b81a93495faec72e4261af0a2
biggiebk <81452178+biggiebk@users.noreply.github.com>
2021-03-28 12:36:24 -0700

    Delete subDir.py

--- subDir.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-from numpy import random
-import os
-import sys
-rootDir = "/home/pi/Music/%s" %(sys.argv[1])
-directory = random.choice(os.listdir(rootDir))
-media = random.choice(os.listdir("%s/%s" %(rootDir, directory)))
-print("%s/%s/%s" %(rootDir, directory, media))
